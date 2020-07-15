7739dd40df5e827904565bf72103897b73cd5c53
Suryakant Kumar <suryakant.kumar@mountblue.tech>
2020-07-15 19:26:02 +0530

    replace function with class based view for listing and creating questions

--- qa/urls.py
+++ qa/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import ApiQuestionListCreateView
 
 urlpatterns = [
-    path('questions', views.questions_list),
+    path('questions', ApiQuestionListCreateView.as_view()),
+    # path('questions', views.questions_list),
     path('questions/<int:pk>', views.questions_detail),
     path('questions/<int:pk>/answers', views.answers_list)
 ]
--- qa/views.py
+++ qa/views.py
@@ -2,28 +2,33 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.generics import ListCreateAPIView
 
 from .models import Question, Answer
 from .serializers import QuestionSerializer, AnswerSerializer
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET', 'POST'])
-def questions_list(request):
-    if request.method == 'GET':
-        questions = Question.objects.all()
+class ApiQuestionListCreateView(ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    pagination_class = PageNumberPagination
 
-        questions_serializer = QuestionSerializer(questions, many=True)
-        return JsonResponse(questions_serializer.data, safe=False)
+# @api_view(['GET', 'POST'])
+# def questions_list(request):
+#     if request.method == 'GET':
+#         questions = Question.objects.all()
+#         questions_serializer = QuestionSerializer(questions, many=True)
+#         return JsonResponse(questions_serializer.data, safe=False)
 
-    elif request.method == 'POST':
-        questions_data = JSONParser().parse(request)
-        questions_serializer = QuestionSerializer(data=questions_data)
-
-        if questions_serializer.is_valid():
-            questions_serializer.save()
-            return JsonResponse(questions_serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(questions_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'POST':
+#         questions_data = JSONParser().parse(request)
+#         questions_serializer = QuestionSerializer(data=questions_data)
+#         if questions_serializer.is_valid():
+#             questions_serializer.save()
+#             return JsonResponse(questions_serializer.data, status=status.HTTP_201_CREATED)
+#         return JsonResponse(questions_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
